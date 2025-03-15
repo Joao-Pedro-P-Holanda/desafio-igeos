696236290ee5d0d56aaa7be71198580d6a556c31
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-03-15 15:03:28 -0300

    refactor(backend): remove unused auth route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from .routes import auth, subsystems
+from .routes import subsystems
 import uvicorn
 
 app = FastAPI(
     title="SIN Dashboard API",
 )
 
-
-app.include_router(auth.router, tags=["Autenticação"])
 app.include_router(subsystems.router, prefix="/subsistemas", tags=["Subsistemas"])
