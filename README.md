6e37c7f51836c1a4af5e9fc3d55ef5b9bacae736
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-03-17 00:11:27 -0300

    feat(backend): create application models
    define models based in the open data present, with partial normalization
    
    create subsystem table with string id and name
    
    create different tables for marginal cost calculated hourly and
    half-hourly, and two other tables for production statements with the
    same calculation frequency
    
    create table for power plant variable cost

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ Fornecer uma forma simplificada de acessar dados das Estimativas de custos da op
 ## Decisões
 
 - **Endpoints em português:** pelo fato da API conter dados referentes a um contexto específico de uma empresa nacional, optei por descrever os endpoints da aplicação em português, para seguir o padrão de outras APIs constantemente utilizadas para a ciência de dados, como a do IBGE e também facilitar a consulta e reduzir a necessidade de memorização por engenheiros de dados interessados.
+- **Importar dados do BigQuery em outro banco:**
+  - **Prós**
+    - Maior simplicidade de integração, permitindo o uso de um ORM.
+  - **Contras**
+    - Possibilidade de inconsistência
 - **Material UI em vez de Tailwind CSS:** o Material UI é uma biblioteca mais robusta, com componentes seguindo padrões de acessibilidade e pré-estilizados. Considerei mais fácil de aplicar para essa aplicação do que utilizar o Tailwind, principalmente pela existência de uma extensão da biblioteca para gerar gráficos.
 
 
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,93 @@
-# TODO: fill with the necessary models if sqlalchemy is used
+from datetime import date, time
+
+from sqlalchemy import ForeignKey, MetaData
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    MappedAsDataclass,
+    mapped_column,
+)
+
+# naming conventions make database migrations easier in multiple databases
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "pk_%(table_name)s",
+}
+
+commom_metadata = MetaData(naming_convention=convention)
+
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    metadata = commom_metadata
+
+
+class SubSystem(Base):
+    __tablename__ = "subsistema"
+    id_subsistema: Mapped[str] = mapped_column(primary_key=True)
+    nome_subsistema: Mapped[str] = mapped_column()
+
+
+class WeeklySubSystemMarginalCost(Base):
+    __tablename__ = "custo_marginal_operacao_semanal"
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id_subsistema: Mapped[int] = mapped_column(ForeignKey("subsistema.id_subsistema"))
+
+    custo_marginal_operacao_semanal: Mapped[float] = mapped_column()
+    custo_marginal_operacao_semanal_carga_leve: Mapped[float] = mapped_column()
+    custo_marginal_operacao_semanal_carga_media: Mapped[float] = mapped_column()
+    custo_marginal_operacao_semanal_carga_pesada: Mapped[float] = mapped_column()
+
+    data: Mapped[date] = mapped_column()
+
+
+class HalfHourlySubSystemMarginalCost(Base):
+    __tablename__ = "custo_marginal_operacao_semihorario"
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id_subsistema: Mapped[str] = mapped_column(ForeignKey("subsistema.id_subsistema"))
+
+    custo_marginal_operacao: Mapped[float] = mapped_column()
+
+    data: Mapped[date] = mapped_column()
+    hora: Mapped[time] = mapped_column()
+
+
+class PowerPlantVariableCost(Base):
+    __tablename__ = "custo_variavel_unitario_usinas_termicas"
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id_subsistema: Mapped[str] = mapped_column(ForeignKey("subsistema.id_subsistema"))
+    id_modelo_usina: Mapped[str] = mapped_column()
+    usina: Mapped[str] = mapped_column()
+    semana_operativa: Mapped[str] = mapped_column()
+    custo_variavel_unitario: Mapped[float] = mapped_column()
+
+    data_inicio: Mapped[date] = mapped_column()
+    data_fim: Mapped[date] = mapped_column()
+
+
+class SubSystemProductionStatement(Base):
+    __abstract__ = True
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id_subsistema: Mapped[str] = mapped_column(ForeignKey("subsistema.id_subsistema"))
+
+    data: Mapped[date] = mapped_column()
+    hora: Mapped[time] = mapped_column()
+
+    geracao_eolica: Mapped[float | None] = mapped_column()
+    geracao_termica: Mapped[float | None] = mapped_column()
+    geracao_solar: Mapped[float | None] = mapped_column()
+    geracao_hidraulica: Mapped[float | None] = mapped_column()
+
+
+class HourlySubSystemProductionStatement(SubSystemProductionStatement):
+    __tablename__ = "balanco_subsistema_horario"
+    valor_carga: Mapped[float] = mapped_column()
+    valor_intercambio: Mapped[float] = mapped_column()
+
+
+class HalfHourlySubSystemProductionStatement(SubSystemProductionStatement):
+    __tablename__ = "balanco_subsistema_semihorario"
+    geracao_hidraulica_pequena_usina: Mapped[float] = mapped_column()
+    geracao_termica_pequena_usina: Mapped[float] = mapped_column()
